7630838e7fe6d23714c3e076794225ec88c3cb8d
andrmaz <mazbsorz@gmail.com>
2021-10-02 11:33:24 +0200

    chore(client config): installed emotion and react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,57 @@
 import * as React from 'react'
 
-function App(): JSX.Element {
-	return <div className='App'></div>
+import {Link, Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+
+import styled from '@emotion/styled'
+
+const Title = styled.h2<{primary?: boolean}>`
+  color: ${props => (props.primary ? 'hotpink' : 'turquoise')};
+`
+
+export default function App(): JSX.Element {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/users'>
+            <Users />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App
+function Home(): JSX.Element {
+  return <Title primary>Home</Title>
+}
+
+function About(): JSX.Element {
+  return <h2>About</h2>
+}
+
+function Users(): JSX.Element {
+  return <h2>Users</h2>
+}
